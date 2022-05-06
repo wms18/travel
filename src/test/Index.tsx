9b12017eedb448f8400f963aa9b9306a9c95857b
wms001 <1532601761@qq.com>
2022-05-06 10:30:21 +0800

    test

---
+++ src/test/Index.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion:
+ * @version: X3版本
+ * @Author: 吴毛三
+ * @Date: 2022-05-06 09:53:51
+ * @LastEditors: 吴毛三
+ * @LastEditTime: 2022-05-06 10:28:34
+ */
+import React from "react";
+
+const Index: React.FC = () => {
+  return <div>789</div>;
+};
+
+export default Index;

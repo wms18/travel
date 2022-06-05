1aa337ce0f4ea081601adced03cabef5b342951b
wms001 <1532601761@qq.com>
2022-06-05 20:42:49 +0800

    redux

---
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion:
+ * @version: X3版本
+ * @Author: 吴毛三
+ * @Date: 2022-06-04 23:03:27
+ * @LastEditors: 吴毛三
+ * @LastEditTime: 2022-06-04 23:07:59
+ */
+import React from "react";
+
+export const ShoppingCart: React.FC = () => {
+  return <div>shoppingCart</div>;
+};
---
+++ src/pages/shoppingCart/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion:
+ * @version: X3版本
+ * @Author: 吴毛三
+ * @Date: 2022-06-04 23:02:48
+ * @LastEditors: 吴毛三
+ * @LastEditTime: 2022-06-04 23:10:22
+ */
+export * from "./ShoppingCart";
